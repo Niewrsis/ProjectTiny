86f06d72618b1b21eb98ff2820d0738790a11ca1
anteavirus <sergeykonovalov2004@gmail.com>
2024-10-06 16:05:00 +0500

    feat: title menu dressed up and (mostly) operational
    + title menu is now kinda cute
    + bloated UIeffects which has some uses but mostly is only for the blur lmao

--- Assets/_Source/Game/TitleScreen/TitleScreenUI.cs
+++ Assets/_Source/Game/TitleScreen/TitleScreenUI.cs
@@ -1,19 +1,33 @@
+using Coffee.UIEffects;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TitleScreenUI : MonoBehaviour
 {
+    public GameObject TitleScreen;
+    public GameObject SettingsScreen;
+    public GameObject TheBackgroundImage;
+    public GameObject SoundVolumeGameObj;
+    public GameObject MusicVolumeGameObj;
+
+
+    private UIEffect BackgroundsEffect;
+    private Slider SoundVolumeSlider;
+    private Slider MusicVolumeSlider;
+
     public void StartGame() 
     {
         SceneManager.LoadScene("SampleScene");
         Debug.Log("please know that TitleScreenUI leads to SampleScene");
     }
 
-    public void Fart()
+    public void Settings()
     {
-        Debug.Log("Implement fart please");
+        SettingsToggled(true);
     }
 
     public void Exit()
@@ -23,4 +37,26 @@ public class TitleScreenUI : MonoBehaviour
         Application.Quit();
     }
 
+    public void SettingsToggled(bool amISettings)
+    {
+        BackgroundsEffect = TheBackgroundImage.GetComponent<UIEffect>();
+        SoundVolumeSlider = SoundVolumeGameObj.GetComponent<Slider>();
+        MusicVolumeSlider = MusicVolumeGameObj.GetComponent<Slider>();
+        if (amISettings)
+        {
+            TitleScreen.SetActive(false);
+            SettingsScreen.SetActive(true);
+            BackgroundsEffect.blurFactor = 1.0f;
+        }
+        else
+        {
+            TitleScreen.SetActive(true);
+            SettingsScreen.SetActive(false);
+            BackgroundsEffect.blurFactor = 0f;
+        }
+        PlayerPrefs.SetFloat("sfx_volume", SoundVolumeSlider.value);
+        PlayerPrefs.SetFloat("mus_volume", MusicVolumeSlider.value);
+        PlayerPrefs.Save();
+        // TODO implement volume manager IN GAME.
+    }
 }
