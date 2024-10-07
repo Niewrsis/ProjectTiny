f619ffb8ec980b58d3442620886836614acf882a
Niewrsis <154369161+Niewrsis@users.noreply.github.com>
2024-10-07 12:27:54 +0300

    fix: water&player
    = fixed player size
    = fixed player jump force
    + added water

--- Assets/_Source/Game/CameraSystem/CameraFollow.cs
+++ Assets/_Source/Game/CameraSystem/CameraFollow.cs
@@ -6,15 +6,12 @@ namespace Game.CameraSystem
 {
     public class CameraFollow : MonoBehaviour
     {
-        //Main character
         [SerializeField] private Transform target;
-        //Camera speed
         [SerializeField] private float speed;
 
         private void Update()
         {
-            //Transform position of camera
-            transform.position = Vector3.Lerp(new Vector3(transform.position.x, transform.position.y, -10), target.position, speed * Time.deltaTime);
+            transform.position = Vector3.Lerp(new Vector3(transform.position.x, transform.position.y, -20), target.position, speed * Time.deltaTime);
         }
     }
 }
