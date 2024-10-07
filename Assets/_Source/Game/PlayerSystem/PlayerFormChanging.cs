82ca1536bd3c7a369bc1cf1d7926899d3c2deb59
Niewrsis <154369161+Niewrsis@users.noreply.github.com>
2024-10-07 16:08:41 +0300

    fix: clearing debugs

--- Assets/_Source/Game/PlayerSystem/PlayerFormChanging.cs
+++ Assets/_Source/Game/PlayerSystem/PlayerFormChanging.cs
@@ -13,12 +13,10 @@ namespace Game.PlayerSystem
             if (player.FormID == 2)
             {
                 player.FormID = 0;
-                Debug.Log($"Form ID - {player.FormID}");
             }
             else
             {
                 player.FormID++;
-                Debug.Log($"Form ID - {player.FormID}");
             }
             _formView.ChangeForm(player.FormID, playerSprite);
         }
